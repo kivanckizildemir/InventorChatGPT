e954c61b770fa6ce21581590b4eebdb3f4c428ea
kivanckizildemir <48845235+kivanckizildemir@users.noreply.github.com>
2023-11-01 23:36:45 +0300

    Repo created

---
+++ MyForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace InventorChatGPT
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputTextBox.Location = new System.Drawing.Point(408, 67);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.outputTextBox.Size = new System.Drawing.Size(380, 371);
+            this.outputTextBox.TabIndex = 1;
+            this.outputTextBox.Text = "";
+            // 
+            // submitButton
+            // 
+            this.submitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.submitButton.Location = new System.Drawing.Point(263, 341);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(128, 42);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.inputTextBox.Location = new System.Drawing.Point(11, 67);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.inputTextBox.Size = new System.Drawing.Size(380, 268);
+            this.inputTextBox.TabIndex = 4;
+            this.inputTextBox.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::InventorChatGPT.Properties.Resources.au2023_speaker_cobranded_social_media_banner_1000x390px_1687471166010001tGwK;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 341);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(245, 97);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Input here";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(405, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Output";
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.outputTextBox);
+            this.Name = "MyForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.RichTextBox inputTextBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MyForm.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using System.Reflection;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace InventorChatGPT
+{
+    public partial class MyForm : Form
+    {
+        const string API_KEY = "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";
+
+        public MyForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void submitButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string request = inputTextBox.Text;
+                if (request.Contains("\n"))
+                    request = request.Replace("\n", " ");
+
+                string invApp = File.ReadAllText("InvApp.txt");
+                request = "Give me a standalone 'public static void Main()' method in C# to " + request + " in current Inventor window.You use this as Inventor.Application object " + invApp + ". Ignore code descriptions, comments and instructions.";
+
+                string endpoint = "https://api.openai.com/v1/chat/completions";
+                var messages = new[]
+                {
+                    new {role = "user", content = request}
+                };
+
+                var data = new
+                {
+                    model = "gpt-4-0613",
+                    messages = messages,
+                    temperature = 0
+                };
+
+                string jsonString = JsonConvert.SerializeObject(data);
+                var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("authorization", "Bearer " + API_KEY);
+                var response2 = await client.PostAsync(endpoint, content);
+                string responseContent = await response2.Content.ReadAsStringAsync();
+                var jsonResponse = JObject.Parse(responseContent);
+                var response = jsonResponse["choices"][0]["message"]["content"].Value<string>();
+
+                string input = response;
+                if (input.IndexOf("```csharp") != -1)
+                    input = input.Substring(input.IndexOf("```csharp") + 10);
+                else if (input.IndexOf("```") != 1)
+                    input = input.Substring(input.IndexOf("```") + 4);
+
+                if (input.IndexOf("```") != -1)
+                    input = input.Substring(0, input.IndexOf("```"));
+
+                if (!input.Contains("using Inventor;\n"))
+                    input = "using Inventor;\n" + input;
+
+                if (!input.Contains("using System;\n"))
+                    input = "using System;\n" + input;
+
+                if (input.Contains("Document newDoc = invApp.Documents.Add(DocumentTypeEnum.kPartDocumentObject, false);"))
+                    input = input.Replace("Document newDoc = invApp.Documents.Add(DocumentTypeEnum.kPartDocumentObject, false);", "Document newDoc = invApp.Documents.Add(DocumentTypeEnum.kPartDocumentObject);");
+
+                if (!input.Contains("public static void Main()") && input.Contains("static void Main()"))
+                    input = input.Replace("static void Main()", "public static void Main()");
+
+                else if (!input.Contains("public static void Main(string[] args)") && input.Contains("static void Main(string[] args)"))
+                    input = input.Replace("static void Main(string[] args)", "public static void Main()");
+
+                StreamWriter sw = new StreamWriter("Input.txt");
+                outputTextBox.Text = input;
+                sw.WriteLine(input);
+                sw.Close();
+
+                CSharpCodeProvider provider = new CSharpCodeProvider();
+                CompilerParameters parameters = new CompilerParameters
+                {
+                    GenerateExecutable = false,
+                    GenerateInMemory = true
+                };
+
+                parameters.ReferencedAssemblies.Add(@"C:\Program Files\Autodesk\Inventor 2023\Bin\Public Assemblies\Autodesk.Inventor.Interop.dll");
+                parameters.ReferencedAssemblies.Add(@"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\System.dll");
+                parameters.ReferencedAssemblies.Add(@"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\System.Windows.Forms.dll");
+                CompilerResults results = provider.CompileAssemblyFromSource(parameters, input);
+
+                if (results.Errors.HasErrors)
+                {
+                    Console.WriteLine("Errors building the code:");
+                    foreach (CompilerError error in results.Errors)
+                    {
+                        Console.WriteLine(error.ToString());
+                        outputTextBox.Text += "\n" + error.ToString();
+                    }
+                }
+                else
+                {
+                    Assembly assembly = results.CompiledAssembly;
+                    Type[] types = assembly.GetTypes();
+                    MethodInfo method = types[0].GetMethod("Main");
+                    method.Invoke(null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
